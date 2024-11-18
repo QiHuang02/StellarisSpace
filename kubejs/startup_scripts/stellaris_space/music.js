012f267eede10ff236905d763c59fdea4e00d09b
QiHuang02 <2830447227@qq.com>
2024-11-18 22:21:57 +0800

    让注册的唱片不可堆叠

--- kubejs/startup_scripts/stellaris_space/music.js
+++ kubejs/startup_scripts/stellaris_space/music.js
@@ -4,7 +4,9 @@ StartupEvents.registry('sound_event', (event) => {
 });
 StartupEvents.registry('item', (event) => {
     event.create('stellaris_space:love_story')
-         .jukeboxPlayable('stellaris_space:love_story', true);
+         .jukeboxPlayable('stellaris_space:love_story', true)
+         .maxStackSize(1);
     event.create('stellaris_space:never_gonna_give_you_up')
-         .jukeboxPlayable('stellaris_space:never_gonna_give_you_up', true);
+         .jukeboxPlayable('stellaris_space:never_gonna_give_you_up', true)
+         .maxStackSize(1);
 })
