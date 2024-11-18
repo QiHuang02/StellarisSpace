d37e280838558c26eab27fa22a872731677774e9
QiHuang02 <2830447227@qq.com>
2024-11-18 22:19:42 +0800

    重命名脚本名称为solar_panel

--- kubejs/server_scripts/stellaris_space/solar_panel.js
+++ kubejs/server_scripts/stellaris_space/solar_panel.js
@@ -1,4 +1,4 @@
-// const $EnergyStorageAttachment$Factory = Java.loadClass(`dev.latvian.mods.kubejs.block.entity.EnergyStorageAttachment$Factory`)
+const $MobEffectInstance = Java.loadClass('net.minecraft.world.effect.MobEffectInstance')
 
 BlockEvents.blockEntityTick('stellaris_space:solar_panel', (event) => {
     let isDay = event.level.day;
@@ -8,9 +8,9 @@ BlockEvents.blockEntityTick('stellaris_space:solar_panel', (event) => {
 
     if(canSeeSky) {
         if(isDay) entity.attachments.energy.addEnergy(500 * 20, false)
-        else if(isNight) event.block.entity.attachments.energy.addEnergy(5 * 20, false)
+        else if(isNight) entity.attachments.energy.addEnergy(5 * 20, false)
     }
-})
+});
 
 BlockEvents.rightClicked('stellaris_space:solar_panel', (event) => {
     let name = event.player.getName();
@@ -22,4 +22,4 @@ BlockEvents.rightClicked('stellaris_space:solar_panel', (event) => {
         // event.player.tell(`Player Name: ${name[2]}`);
         event.player.runCommandSilent(`title @s actionbar [{"text":"MaxEnergy: ","color":"#55FF55"},{"text":" ${cap}","color":"#FFAA00"}]`)
     };
-})
+});
